5cb7df3bdd00442accc2f7b76fb90b65c186e8f4
Ian <kieldro@gmail.com>
2018-05-02 16:38:50 -0400

    structure

---
+++ src/test/jav/junit/AllTests.java
@@ -0,0 +1,15 @@
+package junit;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite(AllTests.class.getName());
+		//$JUnit-BEGIN$
+
+		//$JUnit-END$
+		return suite;
+	}
+
+}
