df1c37119b1df69651f7e5e3dcad659796a5860b
Ian <kieldro@gmail.com>
2018-05-06 10:40:28 -0400

    adds servlet annotation

--- src/main/java/com/revature/servlet/MasterServlet.java
+++ src/main/java/com/revature/servlet/MasterServlet.java
@@ -5,11 +5,12 @@ import java.io.PrintWriter;
 
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-//@WebServlet("/*do")
+@WebServlet(name="MasterServlet", urlPatterns= {"/*.do"})
 public class MasterServlet  extends HttpServlet {
 		private static final long serialVersionUID = 1L;
 	       
