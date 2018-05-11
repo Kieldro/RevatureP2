93533012612069efb0de12b6a6dc1cd25529ccea
Dominick Sermeno <dominick.sermeno@gmail.com>
2018-05-11 11:05:04 -0400

    made changes to angular app
    testing new features on my branch

--- angularApp/src/app/app.component.ts
+++ angularApp/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'RevatureP2';
+  title = 'RevatureP2 - Team Townes';
 }
--- angularApp/src/app/app.module.ts
+++ angularApp/src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { TestPageComponent } from './test-page/test-page.component';
+import { RouterModule, Routes} from '@angular/router';
+
+const appRoutes:Routes=[
+  {path: 'Home', component: HomePageComponent},
+  {path: 'Tests',component:TestPageComponent},
+  {path: '', redirectTo:'/home',pathMatch: 'full' },
+  {path: '**', redirectTo:'/home',pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    TestPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
