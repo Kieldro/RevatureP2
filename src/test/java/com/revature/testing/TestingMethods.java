6a8345fe4aa2d6acced9f6296980011c9a4c25d4
John Eifert <johnreifert94@gmail.com>
2018-05-14 12:58:27 -0400

    hopefully helped get the trainer login test unstuck

--- src/test/java/com/revature/testing/TestingMethods.java
+++ src/test/java/com/revature/testing/TestingMethods.java
@@ -50,7 +50,7 @@ public class TestingMethods
 		
 		/////////////////////////////////////////////////////////////////////
 		String sourceCode = "";
-		for(int i=0; i<75; i++)
+		for(int i=0; i<25; i++)
 		{
 			sourceCode = browser.getPageSource();
 			System.out.println(browser.getCurrentUrl());
