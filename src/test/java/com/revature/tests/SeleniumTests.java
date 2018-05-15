6d9d67afd62c1367caeeb7b2dc5a6ae6569497d4
Ian <kieldro@gmail.com>
2018-05-15 05:02:53 -0400

    complete driver singleton

--- src/test/java/com/revature/tests/SeleniumTests.java
+++ src/test/java/com/revature/tests/SeleniumTests.java
@@ -1,16 +1,43 @@
 package com.revature.tests;
 
-import org.testng.annotations.*;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.revature.util.Driver;
 
 public class SeleniumTests {
+	private static final Driver driver = Driver.getInstance();;
+	
 	@BeforeClass
 	public void setUp() {
 		System.out.println("Starting SeleniumTests... " + SeleniumTests.class.getName());
+		System.out.println("driver: " + driver);
 	}
 
 	@Test(groups = { "group1" })
 	public void test1()
 	{
-		System.out.println("test1() " + SeleniumTests.class.getName());
+		System.out.println("test1() " + "");
+		driver.get("https://www.wikipedia.org");
+
+		// Acquire the relevant input objects:
+		WebElement searchInput = driver.findElement(By.id("searchInput"));
+		WebElement submitButton = driver.findElement(By.className("pure-button-primary-progressive"));
+
+		searchInput.sendKeys("Andromeda Galaxy");
+		submitButton.click();
+		
+//		driver.manage().timeouts().implicitlyWait(300, TimeUnit.SECONDS);
+	}
+	
+	@AfterClass
+	public void tearDown() {
+		System.out.println("Ending SeleniumTests... " + "");
+//		driver.quit();
 	}
 }
--- src/test/java/com/revature/util/Driver.java
+++ src/test/java/com/revature/util/Driver.java
@@ -3,24 +3,36 @@ package com.revature.util;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-public final class Driver {
-	private static final ChromeDriver driver = null;
+// Webdriver Singleton
+public final class Driver extends ChromeDriver {
+	private static Driver driver = null;
 	private static ChromeOptions options;
 
 	private Driver() {
-		System.out.println("Driver constructing...");
-		String args = "user-data-dir=C:\\Users\\Administrator\\AppData\\Local\\Google\\Chrome\\User Data";
-		options = new ChromeOptions().addArguments(args);
 	}
 
-	public static ChromeDriver getInstance() {
-		// Multi-OS support:
-		String os = System.getProperty("os.name");
-		String path = "src/main/resources/chromedriver.exe";
-		if (os.equals("Linux"))
-			path = "src/main/resources/chromedriver";
-		System.setProperty("webdriver.chrome.driver", path);
+	private Driver(ChromeOptions options) {
+		super(options);
+	}
 
-		return new ChromeDriver(options);
+	public static Driver getInstance() {
+		if (driver == null) {
+			String os = System.getProperty("os.name");
+			String path = "src/main/resources/chromedriver.exe";
+			if (os.equals("Linux"))
+				path = "src/main/resources/chromedriver";
+			System.setProperty("webdriver.chrome.driver", path);
+
+			if (true) {
+				driver = new Driver(); // run locally
+			} else {
+				System.out.println("Driver constructing...");
+				String args = "user-data-dir=C:\\Users\\Administrator\\AppData\\Local\\Google\\Chrome\\User Data";
+				options = new ChromeOptions().addArguments(args);
+				driver = new Driver(options);
+			}
+		}
+
+		return driver;
 	}
 }
