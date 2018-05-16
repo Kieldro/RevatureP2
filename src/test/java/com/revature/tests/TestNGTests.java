58073a8b97c02f50369540f8d7793aef5905044f
Jackie Chen <jackiechen5443@gmail.com>
2018-05-15 21:33:07 -0400

    Fixing dependencies of atomized tests.

--- src/test/java/com/revature/tests/TestNGTests.java
+++ src/test/java/com/revature/tests/TestNGTests.java
@@ -379,7 +379,7 @@ public class TestNGTests
 				"https://dev.assignforce.revaturelabs.com/trainers");
 	}
 	
-	@Test(groups="vpTests", dependsOnMethods="vpLoginTest", priority=7)
+	@Test(groups="vpTests", dependsOnMethods="vpTrainersNavTest()", priority=7)
 	public void makeTrainersTest()
 	{
 		String firstName = Integer.toString(rand.nextInt(500000)).concat(
